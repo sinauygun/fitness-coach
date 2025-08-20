4bb21e9fd8a89fdf91f5608cc75ddeac0573a31c
sina <ilyasuygun1@gmail.com>
2025-08-20 18:45:20 +0300

    fix(gh-pages): router basename + relative manifest/SW + start_url

--- src/router.tsx
+++ src/router.tsx
@@ -4,9 +4,12 @@ import CalendarView from "./views/CalendarView";
 import ChartsView from "./views/ChartsView";
 import EditorView from "./views/EditorView";
 
-export const router = createBrowserRouter([
-  { path: "/", element: <App/> },
-  { path: "/calendar", element: <CalendarView/> },
-  { path: "/charts", element: <ChartsView/> },
-  { path: "/editor", element: <EditorView/> }
-]);
+export const router = createBrowserRouter(
+  [
+    { path: "/", element: <App/> },
+    { path: "/calendar", element: <CalendarView/> },
+    { path: "/charts", element: <ChartsView/> },
+    { path: "/editor", element: <EditorView/> }
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
